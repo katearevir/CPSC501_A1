8d8034cee75e5043cbdf03a9ff7c9a73aefddb00
Kate Rivera <kateashley.rivera@ucalgary.ca>
2021-10-10 00:43:41 -0600

    BankAccountTest tests added

--- bank/src/test/java/bank/BankAccountTest.java
+++ bank/src/test/java/bank/BankAccountTest.java
@@ -5,9 +5,54 @@ import org.junit.jupiter.api.Test;
 
 public class BankAccountTest {
     
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    // refactoring added collection methods, 
+    // balance (tested in ChequingAccountTest & SavingsAccountTest), 
+    // and transfer (tested in ChequingAccountTest & SavingsAccountTest)
+
+    public class BankAccClass extends BankAccount
+    { 
+        public BankAccClass(){};
+        public BankAccClass(double initialBalance){super(initialBalance);}
+        public BankAccClass(double initialBalance, String accountNumber) {super(initialBalance,accountNumber);}
+        public BankAccClass(Customer c, double initialBalance){super(c,initialBalance);}
+        public BankAccClass(BankAccount acc){super(acc);}
+        double amount = 0.0;
+        protected double getMonthlyFeesAndInterest() 
+        { 
+            return amount;
+        }
     }
+
+    @Test
+    public void testAddAndGetChequingAccountToCollection()
+    {
+        Customer c1 = new Customer("John Doe", 123);
+        Customer c2 = new Customer("Jane Doe", 321);
+        ChequingAccount john = new ChequingAccount(c1, 1000, 10);
+        ChequingAccount jane = new ChequingAccount(c2, 1000, 10);
+        BankAccount b = new BankAccClass();
+
+        b.addChequingAccountToCollection(john);
+        b.addChequingAccountToCollection(jane);
+
+        assertEquals(john, b.getChequingAccountFromCollection(c1));
+        assertEquals(jane, b.getChequingAccountFromCollection(c2));
+    }
+
+    @Test
+    public void testAddAndGetSavingsAccountToCollection()
+    {
+        Customer c1 = new Customer("John Doe", 123);
+        Customer c2 = new Customer("Jane Doe", 321);
+        SavingsAccount john = new SavingsAccount(c1, 1000);
+        SavingsAccount jane = new SavingsAccount(c2, 1000);
+        BankAccount b = new BankAccClass();
+
+        b.addSavingsAccountToCollection(john);
+        b.addSavingsAccountToCollection(jane);
+
+        assertEquals(john, b.getSavingsAccountFromCollection(c1));
+        assertEquals(jane, b.getSavingsAccountFromCollection(c2));
+    }
+
 }
